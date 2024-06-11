d6c319a3bb7472af82c185fb6646e17ff25da451
Benjamin <benja@wcanvas.com>
2024-06-11 08:45:57 -0300

    Hero and nav fix

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,29 +1,29 @@
 // import Swiper core and required modules
-import { motion } from 'framer-motion';
-import { Navigation } from 'swiper/modules';
+import { Navigation, Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from "swiper/react";
-import { useRef } from 'react';
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/navigation';
 import styles from './Carousel.module.scss';
 
 const Carousel = ({ cards }) => {
-    const sliderRef = useRef();
-
-    return <motion.div initial={{ opacity: 0, y: 100}} whileInView={{ opacity: 1, y: 0}} transition={{duration: .5}} viewport={{ once: true}}>
+    return <div className={styles.carousel}>
     <Swiper 
-        onSwiper={it => sliderRef.current = it}
-        modules={[Navigation]}
+        className={styles.swiper}
+        modules={[Navigation, Autoplay]}
         spaceBetween={50}
         slidesPerView={1}
-        navigation
+        loop
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+
     >
         {cards.map( (card, index) => {
             return <SwiperSlide key={index}><img className={styles.img} src={card.src} alt="fishing image" /></SwiperSlide>
         } )}
     </Swiper>
-    </motion.div>
+    </div>
 }
 
 export default Carousel;
--- src/components/Link/Link.jsx
+++
@@ -1,7 +0,0 @@
-import styles from './Link.module.scss';
-
-const Link = ({ href, target, text, type = 'primary', color = "white" }) => {
-    return <a className={`${styles.link} ${styles[type]} ${styles[color]}`} href={href} target={target}>{text}</a>
-}
-
-export default Link;
