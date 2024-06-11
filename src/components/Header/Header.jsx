49dbb20a7233962d3c74e33010d743302dcf7983
Benjamin <benja@wcanvas.com>
2024-06-11 09:14:41 -0300

    Finish home page

--- src/components/Header/Header.jsx
+++
@@ -1,7 +0,0 @@
-import { motion } from "framer-motion";
-
-const Header = ({ tag: Tag, text, className }) => {
-    return <motion.div initial={{ opacity: 0, y: 80}} whileInView={{ opacity: 1, y: 0}} viewport={{ once: true }} transition={{ duration: .5}}><Tag className={className}>{text}</Tag></motion.div>
-}
-
-export default Header;
